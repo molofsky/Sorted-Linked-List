80543e897a634edeab61965c18174d2e26f433ab
Adrian Molofsky <molofsky@stanford.edu>
2023-11-09 12:49:05 -0800

    Create linkedlist.cpp

---
+++ linkedlist.cpp
@@ -0,0 +1,48 @@
+/*
+* Linked List with insert and delete methods
+*
+* @author Adrian Molofsky
+* @version 11/9/23
+*/
+
+struct Node {
+  int val;
+  Node* next;
+};
+
+struct List {
+  Node* head;
+};
+
+void insert(List* list, int val) {
+  Node* n = new Node;
+  n->val = val;
+
+  Node* prev = list->head;
+  Node* cur = list->head->next;
+
+  while (cur) {
+    if (cur->val > val) break;
+    prev = cur;
+    cur = cur->next;
+  }
+
+  n->next = cur;
+  prev->next = n;
+}
+
+void delete(List* list, int val) {
+  Node* prev = list->head;
+  Node* cur = list->head->next;
+
+  while (cur) {
+    if (cur->val = val) {
+      prev->next = cur->next;
+      delete cur;
+      return;
+    }
+
+    prev = cur;
+    cur = cur->next;
+  }
+}
